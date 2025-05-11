3ce1b0ab2373de294e132cf482bbe663518dd88b
Syed Mohamed Harris P S <syedmdharris@gmail.com>
2025-05-11 19:14:03 +0530

    prediction pipeline added

---
+++ src/cnnClassifier/pipeline/prediction.py
@@ -0,0 +1,39 @@
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import os
+
+
+
+class PredictionPipeline:
+    def __init__(self,filename):
+        self.filename =filename
+
+
+    
+    def predict(self):
+        ## load model
+        
+        # model = load_model(os.path.join("artifacts","training", "model.h5"))
+        model = load_model(os.path.join("model", "model.h5"))
+
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size = (224,224))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = np.argmax(model.predict(test_image), axis=1)
+        print(result)
+
+        if result[0] == 0:
+            prediction = 'Adenocarcinoma Cancer'
+            return [{ "image" : prediction}]
+        elif result[0] == 1:
+            prediction = 'Large Cell Carcinoma Cancer'
+            return [{ "image" : prediction}]
+        elif result[0] == 2:
+            prediction = 'Normal'
+            return [{ "image" : prediction}]
+        elif result[0] == 3:
+            prediction = 'Squamous Cell Cancer'
+            return [{ "image" : prediction}]
+    
